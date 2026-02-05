b9ae92dc0b3e149204a0049b88e2d05522da09e1
gabibag <97263038+gabiminto@users.noreply.github.com>
2026-02-04 21:16:12 -0800

    converted to nextjs

---
+++ app/utils/links.ts
@@ -0,0 +1,16 @@
+export const SOCIAL_LINKS = {
+    github: "https://github.com/gabiminto",
+    instagram: "https://www.instagram.com/gabibag_/",
+};
+
+export const PROJECT_LINKS = {
+    codeForCause: "https://github.com/Gabibag/codeforcause",
+    shareABite: "https://github.com/Gabibag/ShareABite",
+    chemAcademy: "https://github.com/WilyHyperion/ChemAcademy",
+    keepersLand: "https://github.com/Gabibag/KeepersLand",
+    allProjects: "https://github.com/gabiminto?tab=repositories",
+};
+
+export const openLink = (url: string) => {
+    window.open(url, "_blank");
+};
